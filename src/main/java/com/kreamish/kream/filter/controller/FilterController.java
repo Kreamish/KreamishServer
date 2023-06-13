5d8b8d018da3ef572b66806a0ddd7300b4fd9104
seung-woo-ryu <tmddn645@naver.com>
2023-06-13 19:58:15 +0900

    feat: add filter controller

--- src/main/java/com/kreamish/kream/filter/controller/FilterController.java
+++ src/main/java/com/kreamish/kream/filter/controller/FilterController.java
@@ -1,14 +1,19 @@
 package com.kreamish.kream.filter.controller;
 
 import com.kreamish.kream.common.util.ApiUtils;
-import com.kreamish.kream.filter.dto.CategoryDetailFilterResultDto;
+import com.kreamish.kream.filter.dto.CategoriesFilterResultDto;
 import com.kreamish.kream.filter.facade.FilterFacade;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 import static com.kreamish.kream.common.util.ApiUtils.success;
 
@@ -18,7 +23,15 @@ import static com.kreamish.kream.common.util.ApiUtils.success;
 public class FilterController {
     private final FilterFacade filterFacade;
     @GetMapping("/categories")
-    public ResponseEntity<ApiUtils.ApiResult<CategoryDetailFilterResultDto>> getCategories() {
+    @Operation(
+            summary = "카테고리 별 세부 카테고리 리스트 반환",
+            description = "필터링을 위한 모든 카테고리 별 세부 카테고리 리스트 반환"
+    )
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "필터링을 위한 리스트 정상 반환"),
+            @ApiResponse(responseCode = "404")
+    })
+    public ResponseEntity<ApiUtils.ApiResult<List<CategoriesFilterResultDto>>> getCategories() {
         return new ResponseEntity<>(success(filterFacade.getCategories()), HttpStatus.OK);
     }
 }
