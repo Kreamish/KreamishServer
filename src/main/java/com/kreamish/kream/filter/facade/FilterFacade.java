b5cad7fb2f960a001a57413d7b9f8a7dcf5db7fe
seung-woo-ryu <tmddn645@naver.com>
2023-06-13 19:58:28 +0900

    feat: add filter facade

--- src/main/java/com/kreamish/kream/filter/facade/FilterFacade.java
+++ src/main/java/com/kreamish/kream/filter/facade/FilterFacade.java
@@ -1,20 +1,31 @@
 package com.kreamish.kream.filter.facade;
 
-import com.kreamish.kream.category.dto.CategoriesDto;
+import com.kreamish.kream.category.dto.CategoryDto;
 import com.kreamish.kream.category.service.CategoryService;
-import com.kreamish.kream.categorydetail.dto.CategoryDetailsDto;
+import com.kreamish.kream.categorydetail.dto.CategoryDetailDto;
 import com.kreamish.kream.categorydetail.service.CategoryDetailService;
-import com.kreamish.kream.filter.dto.CategoryDetailFilterResultDto;
+import com.kreamish.kream.filter.dto.CategoriesFilterResultDto;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
+@Component
+@Transactional
 public class FilterFacade {
+
     private final CategoryService categoryService;
     private final CategoryDetailService categoryDetailService;
-    public CategoryDetailFilterResultDto getCategories() {
-        CategoriesDto allCategories = categoryService.getAllCategories();
-        CategoryDetailsDto allCategoryDetails = categoryDetailService.getAllCategoryDetails();
 
-        return CategoryDetailFilterResultDto.of(allCategories, allCategoryDetails);
+    public List<CategoriesFilterResultDto> getCategories() {
+        List<CategoryDto> categoryDtoList = categoryService.getAllCategories().getCategoryDtoList();
+        List<CategoryDetailDto> categoryDetailDtoList = categoryDetailService.getAllCategoryDetails()
+            .getCategoryDetailDtoList();
+
+        return categoryDtoList.stream()
+            .map(categoryDto -> CategoriesFilterResultDto.of(categoryDto, categoryDetailDtoList))
+            .collect(Collectors.toList());
     }
 }
