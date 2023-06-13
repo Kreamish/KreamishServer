72b48d3ae069ee753b37486928c4498b2b444ecd
kangyunsik <re12io@ajou.ac.kr>
2023-06-14 00:17:25 +0900

    fix: test 코드 application-local.yaml 주석 처리

--- src/test/java/com/kreamish/kream/KreamApplicationTests.java
+++ src/test/java/com/kreamish/kream/KreamApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {"spring.config.location = classpath:application-local.yaml"})
+//@TestPropertySource(properties = {"spring.config.location = classpath:application-local.yaml"})
 class KreamApplicationTests {
 
     @Test
