6d1e5a68e93012b78f0e16fc679dab14f9b221b4
seung-woo-ryu <tmddn645@naver.com>
2023-06-19 22:42:25 +0900

    feat: add getCollections method

--- src/main/java/com/kreamish/kream/filter/controller/FilterController.java
+++ src/main/java/com/kreamish/kream/filter/controller/FilterController.java
@@ -3,6 +3,7 @@ package com.kreamish.kream.filter.controller;
 import static com.kreamish.kream.common.util.ApiUtils.success;
 
 import com.kreamish.kream.brand.dto.BrandDto;
+import com.kreamish.kream.collection.dto.CollectionDto;
 import com.kreamish.kream.common.util.ApiUtils.ApiResult;
 import com.kreamish.kream.filter.dto.CategoriesFilterResultDto;
 import com.kreamish.kream.filter.facade.FilterFacade;
@@ -43,12 +44,24 @@ public class FilterController {
         summary = "브랜드 리스트 반환",
         description = "필터링을 위한 모든 브랜드 리스트 반환"
     )
-
     @ApiResponses({
         @ApiResponse(responseCode = "200", description = "필터링을 위한 리스트 정상 반환"),
         @ApiResponse(responseCode = "404")
     })
     public ResponseEntity<ApiResult<List<BrandDto>>> getBrand() {
         return new ResponseEntity<>(success(filterFacade.getBrand()), HttpStatus.OK);
+    }
+
+    @GetMapping("/collections")
+    @Operation(
+        summary = "컬렉션 리스트 반환",
+        description = "필터링을 위한 모든 컬렉션 리스트 반환"
+    )
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "필터링을 위한 리스트 정상 반환"),
+        @ApiResponse(responseCode = "404")
+    })
+    public ResponseEntity<ApiResult<List<CollectionDto>>> getCollections() {
+        return new ResponseEntity<>(success(filterFacade.getCollections()), HttpStatus.OK);
     }
 }
