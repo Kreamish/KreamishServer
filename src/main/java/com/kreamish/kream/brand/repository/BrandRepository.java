a2bc817f525f6eb88381d2f679ed1cffa3785c6d
seung-woo-ryu <tmddn645@naver.com>
2023-06-25 17:56:55 +0900

    fix: remove unnecessary implements

--- src/main/java/com/kreamish/kream/brand/repository/BrandRepository.java
+++ src/main/java/com/kreamish/kream/brand/repository/BrandRepository.java
@@ -1,9 +1,8 @@
 package com.kreamish.kream.brand.repository;
 
 import com.kreamish.kream.brand.entity.Brand;
-import com.kreamish.kream.legacy.repository.ItemQueryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BrandRepository extends JpaRepository<Brand, Long>, ItemQueryRepository {
+public interface BrandRepository extends JpaRepository<Brand, Long> {
 
 }
--- src/main/java/com/kreamish/kream/collection/repository/CollectionRepository.java
+++ src/main/java/com/kreamish/kream/collection/repository/CollectionRepository.java
@@ -1,9 +1,8 @@
 package com.kreamish.kream.collection.repository;
 
 import com.kreamish.kream.collection.entity.Collection;
-import com.kreamish.kream.legacy.repository.ItemQueryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CollectionRepository extends JpaRepository<Collection, Long>, ItemQueryRepository {
+public interface CollectionRepository extends JpaRepository<Collection, Long> {
 
 }
