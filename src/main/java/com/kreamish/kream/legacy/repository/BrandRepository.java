687d3ee23d71ad060dd6c4ab358c962b597d1b39
kangyunsik <re12io@ajou.ac.kr>
2023-06-25 16:32:47 +0900

    KS-16 fix: Repository 들에 있던 불필요한 ItemQueryRepository 제거

--- src/main/java/com/kreamish/kream/legacy/repository/BrandRepository.java
+++ src/main/java/com/kreamish/kream/legacy/repository/BrandRepository.java
@@ -3,6 +3,6 @@ package com.kreamish.kream.legacy.repository;
 import com.kreamish.kream.legacy.entity.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BrandRepository extends JpaRepository<Brand, Long>, ItemQueryRepository {
+public interface BrandRepository extends JpaRepository<Brand, Long> {
 
 }
--- src/main/java/com/kreamish/kream/legacy/repository/ItemSizesRepository.java
+++ src/main/java/com/kreamish/kream/legacy/repository/ItemSizesRepository.java
@@ -3,6 +3,6 @@ package com.kreamish.kream.legacy.repository;
 import com.kreamish.kream.legacy.entity.ItemSizes;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemSizesRepository extends JpaRepository<ItemSizes, Long>, ItemQueryRepository {
+public interface ItemSizesRepository extends JpaRepository<ItemSizes, Long> {
 
 }
