b57c97769db0e72b9b3bd78185afe9d7caffec89
kangyunsik <re12io@ajou.ac.kr>
2023-06-25 16:32:47 +0900

    KS-16 fix: Repository 들에 있던 불필요한 ItemQueryRepository 제거

--- src/main/java/com/kreamish/kream/legacy/repository/ItemCollectionRelRepository.java
+++ src/main/java/com/kreamish/kream/legacy/repository/ItemCollectionRelRepository.java
@@ -3,7 +3,6 @@ package com.kreamish.kream.legacy.repository;
 import com.kreamish.kream.legacy.entity.ItemCollectionRel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemCollectionRelRepository extends JpaRepository<ItemCollectionRel, Long>,
-    ItemQueryRepository {
+public interface ItemCollectionRelRepository extends JpaRepository<ItemCollectionRel, Long> {
 
 }
