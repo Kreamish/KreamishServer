a77bbb0cab9e07e4d1f4b6b1d2570435db0fd6c1
kangyunsik <re12io@ajou.ac.kr>
2023-06-27 20:06:09 +0900

    KS-16 docs: Swagger 문서 작성

--- src/main/java/com/kreamish/kream/item/controller/ItemController.java
+++ src/main/java/com/kreamish/kream/item/controller/ItemController.java
@@ -7,6 +7,8 @@ import com.kreamish.kream.item.dto.ItemListResponseDto;
 import com.kreamish.kream.item.dto.ItemListSearchCondition;
 import com.kreamish.kream.item.service.ItemService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,6 +33,10 @@ public class ItemController {
         summary = "필터링 적용한 상품 목록 반환",
         description = "카테고리, 브랜드, 컬렉션 id, 최소/최대 금액 등을 적용한 페이징 처리된 상품 목록 반환"
     )
+    @ApiResponses({
+        @ApiResponse(responseCode = "204", description = "존재하지 않는 리소스"),
+        @ApiResponse(responseCode = "200", description = "정상 반환")
+    })
     public ResponseEntity<ApiUtils.ApiResult<ItemListResponseDto>> getItems(
         @RequestParam(value = "category-id", required = false) List<Long> categoryIds,
         @RequestParam(value = "brand-id", required = false) List<Long> brandIds,
