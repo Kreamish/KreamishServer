5bc3ba16096cd9f1312c08617b10cbb183741093
kangyunsik <re12io@ajou.ac.kr>
2023-07-02 14:17:33 +0900

    feat/KS-17 feat: ItemDetail 내부 로직 작성

--- src/main/java/com/kreamish/kream/item/service/ItemServiceImpl.java
+++ src/main/java/com/kreamish/kream/item/service/ItemServiceImpl.java
@@ -1,10 +1,18 @@
 package com.kreamish.kream.item.service;
 
+import com.kreamish.kream.common.entity.BaseEntity;
+import com.kreamish.kream.item.dto.ItemDetailResponseDto;
 import com.kreamish.kream.item.dto.ItemListResponseDto;
 import com.kreamish.kream.item.dto.ItemListResponsePageDto;
 import com.kreamish.kream.item.dto.ItemListSearchCondition;
 import com.kreamish.kream.item.entity.Item;
+import com.kreamish.kream.item.entity.ItemSizes;
 import com.kreamish.kream.item.repository.ItemRepository;
+import com.kreamish.kream.item.repository.TradeRepository;
+import com.kreamish.kream.legacy.entity.Trade;
+import com.kreamish.kream.legacy.repository.ItemSizesRepository;
+import java.util.Comparator;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -17,6 +25,10 @@ public class ItemServiceImpl implements ItemService {
 
     private final ItemRepository itemRepository;
 
+    private final ItemSizesRepository itemSizesRepository;
+
+    private final TradeRepository tradeRepository;
+
     @Override
     public ItemListResponseDto findItemsByCondition(ItemListSearchCondition condition, PageRequest pageRequest) {
         ItemListResponseDto dto = new ItemListResponseDto();
@@ -24,9 +36,23 @@ public class ItemServiceImpl implements ItemService {
         return dto;
     }
 
+    @Override
+    public ItemDetailResponseDto findItemById(Long itemId) {
+        Item item = itemRepository.findById(itemId)
+            .orElseThrow(() -> new NoSuchElementException("item id로 해당 상품을 찾을 수 없습니다."));
+
+        return ItemDetailResponseDto.builder()
+            .name(item.getName())
+            .subName(item.getSubName())
+            .recentPrice(getRecentPrice(item))
+            .modelCode(item.getModelCode())
+            .releaseDate(item.getReleaseDate())
+            .representativeColor(item.getRepresentativeColor())
+            .build();
+    }
+
     /**
-     * recentPrice, likeCount, commentCount 추후 업데이트 예정.
-     * recentPrice 의 경우 다소 복잡할 것으로 생각
+     * recentPrice, likeCount, commentCount 추후 업데이트 예정. recentPrice 의 경우 다소 복잡할 것으로 생각
      */
     private ItemListResponsePageDto convert(Item entity) {
         return ItemListResponsePageDto.builder()
@@ -34,10 +60,21 @@ public class ItemServiceImpl implements ItemService {
             .brandName(entity.getBrand().getName())
             .name(entity.getName())
             .subName(entity.getSubName())
-            .recentPrice(null)
+            .recentPrice(getRecentPrice(entity))
             .likeCount(null)
             .commentCount(null)
             .imageUrl(entity.getImageUrl())
             .build();
+    }
+
+    private Long getRecentPrice(Item entity) {
+        return tradeRepository.findTradeByItemSizesIdList(
+            itemSizesRepository.findByItemId(entity.getItemId()).stream()
+                .mapToLong(ItemSizes::getItemSizesId)
+                .boxed()
+                .toList()
+        ).stream().max(
+            Comparator.comparing(BaseEntity::getCreatedAt)
+        ).map(Trade::getTradePrice).orElse(null);
     }
 }
