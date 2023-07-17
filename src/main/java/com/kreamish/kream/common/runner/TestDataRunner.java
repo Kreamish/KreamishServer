220213dba1c19aaba910350808869306270aec49
seung-woo-ryu <tmddn645@naver.com>
2023-07-17 16:23:09 +0900

    KS-24 add: add applicationRunner that creates test data

---
+++ src/main/java/com/kreamish/kream/common/runner/TestDataRunner.java
@@ -0,0 +1,16 @@
+package com.kreamish.kream.common.runner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Profile("test")
+@Component
+public class TestDataRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // ToDo : 필요한 데이터들 생성
+    }
+}
