bdfb81cf59d0573f9cabc9f8e7310145365af496
seung-woo-ryu <tmddn645@naver.com>
2023-07-23 16:24:00 +0900

    KS-20 fix: 잘못 쓰여진 import문, 필요없는 메서드 제거

--- src/main/java/com/kreamish/kream/member/repository/MemberRepository.java
+++ src/main/java/com/kreamish/kream/member/repository/MemberRepository.java
@@ -1,10 +1,8 @@
 package com.kreamish.kream.member.repository;
 
 import com.kreamish.kream.member.entity.Member;
-import com.kreamish.kream.member.entity.MemberRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    void save(MemberRole build);
 }
