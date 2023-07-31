53b0b8083a16fb6ef89ba0e62d42978610bfbbc2
kangyunsik <re12io@ajou.ac.kr>
2023-07-31 22:47:18 +0900

    KS-26 chore: ItemController swagger docs 작성

--- src/main/java/com/kreamish/kream/item/controller/ItemController.java
+++ src/main/java/com/kreamish/kream/item/controller/ItemController.java
@@ -7,7 +7,10 @@ import com.kreamish.kream.item.dto.ItemListResponseDto;
 import com.kreamish.kream.item.dto.ItemListSearchCondition;
 import com.kreamish.kream.item.service.ItemService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
@@ -36,9 +39,52 @@ public class ItemController {
         @ApiResponse(responseCode = "204", description = "존재하지 않는 리소스"),
         @ApiResponse(responseCode = "200", description = "정상 반환")
     })
-    @Parameters
+    @Parameters({
+        @Parameter(name = "category-ids", description = "카테고리 id 목록.",
+            examples = {
+                @ExampleObject(name = "multiple", value = "1,2,3", summary = "1,2,3", description = "category-id 가 [1,2,3] 에 속하는 item 필터링"),
+                @ExampleObject(name = "singleton", value = "1", summary = "1", description = "category-id 가 1 인 item 필터링"),
+                @ExampleObject(name = "none", summary = "조건 없는 경우", description = "모든 category-id 로 조회")
+            }
+        ),
+        @Parameter(name = "brand-ids", description = "브랜드 id 목록.",
+            examples = {
+                @ExampleObject(name = "multiple", value = "1,2,3", summary = "1,2,3", description = "brand-id 가 [1,2,3] 에 속하는 item 필터링"),
+                @ExampleObject(name = "singleton", value = "1", summary = "1", description = "brand-id 가 1인  item 필터링"),
+                @ExampleObject(name = "none", summary = "조건 없는 경우", description = "모든 brand-id 로 조회")
+            }
+        ),
+        @Parameter(name = "collection-ids", description = "컬렉션 id 목록.",
+            examples = {
+                @ExampleObject(name = "multiple", value = "1,2,3", summary = "1,2,3", description = "collection-id 가 [1,2,3] 에 속하는 item 필터링"),
+                @ExampleObject(name = "singleton", value = "1", summary = "1", description = "collection-id 가 1인  item 필터링"),
+                @ExampleObject(name = "none", summary = "조건 없는 경우", description = "모든 collection-id 로 조회")
+            }
+        ),
+        @Parameter(name = "min-price", description = "최소 가격 (최근 거래 기준) 현재 데이터가 없어, 반영 X", deprecated = true,
+            examples = {
+                @ExampleObject(name = "none", summary = "조건 없는 경우", description = "가격 필터링 적용 X"),
+//                @ExampleObject(name = "singleton", value = "150000", summary = "min-price: 150000", description = "최소 가격")
+            }
+        ),
+        @Parameter(name = "max-price", description = "최대 가격 (최근 거래 기준) 현재 데이터가 없어, 반영 X", deprecated = true,
+            examples = {
+                @ExampleObject(name = "none", summary = "조건 없는 경우", description = "가격 필터링 적용 X"),
+//                @ExampleObject(name = "singleton", value = "200000", summary = "max-price: 150000", description = "최대 가격")
+            }
+        ),
+        @Parameter(name = "sizes", description = "size 목록. 현재 데이터가 없어, 반영 X", deprecated = true,
+            examples = {
+//                @ExampleObject(name = "multiple", value = "95,100,105", summary = "min-price: 150000", description = "sizes 가 [95,100,105] 속하는 경우"),
+//                @ExampleObject(name = "singleton", value = "100", summary = "min-price: 150000", description = "sizes 가 100 인 경우."),
+                @ExampleObject(name = "none", summary = "조건 없는 경우", description = "size 필터링 적용 X")
+            }
+        ),
+        @Parameter(name = "page", description = "받고자 하는 페이지의 번호 default: 0"),
+        @Parameter(name = "size", description = "한 페이지 당 개수 default: 10")
+    })
     public ResponseEntity<ApiUtils.ApiResult<ItemListResponseDto>> getItems(
-        ItemListSearchCondition condition
+        @Schema(hidden = true) ItemListSearchCondition condition
     ) {
         PageRequest pageRequest = PageRequest.of(condition.getPage(), condition.getPageSize());
 
