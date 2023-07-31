32e54042c2fe1725e6e3099e078531fdbdfac102
kangyunsik <re12io@ajou.ac.kr>
2023-07-31 22:42:06 +0900

    KS-26 refact: argument resolver 재사용 가능한 메서드 분리

--- src/main/java/com/kreamish/kream/item/resolver/ItemGetItemsArgumentResolver.java
+++ src/main/java/com/kreamish/kream/item/resolver/ItemGetItemsArgumentResolver.java
@@ -1,22 +1,17 @@
 package com.kreamish.kream.item.resolver;
 
+import com.kreamish.kream.common.resolver.CommonArgumentResolver;
 import com.kreamish.kream.item.dto.ItemListSearchCondition;
 import jakarta.servlet.http.HttpServletRequest;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
 import java.util.function.Function;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 @Component
-public class ItemGetItemsArgumentResolver implements HandlerMethodArgumentResolver {
+public class ItemGetItemsArgumentResolver extends CommonArgumentResolver {
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -39,31 +34,11 @@ public class ItemGetItemsArgumentResolver implements HandlerMethodArgumentResolv
             .categoryIds(commaStringToList(httpServletRequest.getParameter("category-ids"), parseLong))
             .brandIds(commaStringToList(httpServletRequest.getParameter("brand-ids"), parseLong))
             .collectionIds(commaStringToList(httpServletRequest.getParameter("collection-ids"), parseLong))
-            .minPrice(getOrElseDefault(httpServletRequest.getParameter("min-price"), parseLong, null, false))
-            .maxPrice(getOrElseDefault(httpServletRequest.getParameter("max-price"), parseLong, null, false))
+            .minPrice(getOrElseDefault(httpServletRequest.getParameter("min-price"), parseLong, null))
+            .maxPrice(getOrElseDefault(httpServletRequest.getParameter("max-price"), parseLong, null))
             .sizes(commaStringToList(httpServletRequest.getParameter("sizes"), itSelf))
             .page(getOrElseDefault(httpServletRequest.getParameter("page"), parseInt, 0, true))
-            .pageSize(getOrElseDefault(httpServletRequest.getParameter("page-size"), parseInt, 10, false))
+            .pageSize(getOrElseDefault(httpServletRequest.getParameter("page-size"), parseInt, 10))
             .build();
-    }
-
-    private <T> T getOrElseDefault(String value, Function<String, T> parser, T i, boolean zeroAble) {
-        if (value == null || (!zeroAble && "0".equals(value))) {
-            return i;
-        }
-        return parser.apply(value);
-    }
-
-    private <T> List<T> commaStringToList(String commaString, Function<String, T> parser) {
-        if (commaString == null) {
-            return null;
-        }
-        StringTokenizer st = new StringTokenizer(URLDecoder.decode(commaString, StandardCharsets.UTF_8), ",");
-        List<T> result = new ArrayList<>();
-
-        while (st.hasMoreTokens()) {
-            result.add(parser.apply(st.nextToken()));
-        }
-        return result;
     }
 }
