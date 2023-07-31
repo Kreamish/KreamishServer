41a31a824a6bc82461356a623f800b7cfe809b4a
kangyunsik <re12io@ajou.ac.kr>
2023-07-31 22:40:00 +0900

    KS-26 fix: nullable 제거 (테스트코드 미동작)

--- src/main/java/com/kreamish/kream/common/entity/BaseEntity.java
+++ src/main/java/com/kreamish/kream/common/entity/BaseEntity.java
@@ -1,21 +1,23 @@
 package com.kreamish.kream.common.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 @Getter
+@MappedSuperclass
 public abstract class BaseEntity {
 
-    @Column(name = "created_at", nullable = false, updatable = false)
+    @Column(name = "created_at", updatable = false)
     @CreationTimestamp
     private LocalDateTime createdAt;
     @Column(name = "updated_at")
     @UpdateTimestamp
     private LocalDateTime updatedAt;
-    @Column(name = "create_id", nullable = false, updatable = false)
+    @Column(name = "create_id", updatable = false)
     private String createId;
     @Column(name = "update_id")
     private String updateId;
