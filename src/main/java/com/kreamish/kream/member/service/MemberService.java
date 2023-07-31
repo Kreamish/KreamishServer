9ae0ac3b412731b486b943b261bcb97eb2a8d959
kangyunsik <re12io@ajou.ac.kr>
2023-07-31 22:49:36 +0900

    KS-26 fix: MemberService (기존 인터페이스가 아닌 클래스.) 클래스 직접 주입에서, 인터페이스로 분리. 개행문자 10만줄 제거

--- src/main/java/com/kreamish/kream/member/service/MemberService.java
+++ src/main/java/com/kreamish/kream/member/service/MemberService.java
@@ -2,10 +2,14 @@ package com.kreamish.kream.member.service;
 
 import com.kreamish.kream.member.dto.MemberDetailResponseDto;
 import com.kreamish.kream.member.dto.MemberRegisterRequestDto;
+import com.kreamish.kream.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface MemberService {
+
+    Optional<Member> getMemberById(Long memberId);
 
     Long registerMember(MemberRegisterRequestDto registerDto);
 
