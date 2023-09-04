b6032a381ce7394497d15aa85c4cff449fcaa1e7
seung-woo-ryu <tmddn645@naver.com>
2023-09-04 20:03:30 +0900

    KS-32 add : add LoginMemberArgumentResolver

--- src/main/java/com/kreamish/kream/config/web/WebConfig.java
+++ src/main/java/com/kreamish/kream/config/web/WebConfig.java
@@ -1,6 +1,7 @@
 package com.kreamish.kream.config.web;
 
 import com.kreamish.kream.item.resolver.ItemGetItemsArgumentResolver;
+import com.kreamish.kream.login.resolver.LoginMemberArgumentResolver;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,11 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final ItemGetItemsArgumentResolver itemGetItemsArgumentResolve;
 
+    private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(itemGetItemsArgumentResolve);
+        argumentResolvers.add(loginMemberArgumentResolver);
+
     }
 }
