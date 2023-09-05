e617bcc1b23a9211ebcc0ad37fa5dba88cbd8b2c
kangyunsik <re12io@ajou.ac.kr>
2023-09-05 21:14:37 +0900

    KS-36 feat: Favorite 도메인 추가

---
+++ src/main/java/com/kreamish/kream/favorite/enity/Favorite.java
@@ -0,0 +1,40 @@
+package com.kreamish.kream.favorite.enity;
+
+
+import com.kreamish.kream.common.entity.BaseEntity;
+import com.kreamish.kream.itemsizes.entity.ItemSizes;
+import com.kreamish.kream.member.entity.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Table(name = "favorite")
+@Builder
+public class Favorite extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "favorite_id")
+    private Long favoriteId;
+
+    @ManyToOne
+    @JoinColumn(name = "item_sizes_id", nullable = false)
+    private ItemSizes itemSizes;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+}
