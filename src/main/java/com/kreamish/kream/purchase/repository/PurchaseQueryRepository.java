55c51ba93c0e26d1750a62dcd739ccf1c7d32695
kangyunsik <re12io@ajou.ac.kr>
2023-11-20 20:25:20 +0900

    KS-30 feat: ItemSizes로 조회한 구매 입찰 목록 반환 기능 구현 및 테스트코드 작성

--- src/main/java/com/kreamish/kream/purchase/repository/PurchaseQueryRepository.java
+++ src/main/java/com/kreamish/kream/purchase/repository/PurchaseQueryRepository.java
@@ -1,5 +1,6 @@
 package com.kreamish.kream.purchase.repository;
 
+import com.kreamish.kream.itemsizes.entity.ItemSizes;
 import com.kreamish.kream.member.entity.Member;
 import com.kreamish.kream.purchase.entity.Purchase;
 import jakarta.annotation.Nullable;
@@ -7,4 +8,6 @@ import java.util.List;
 
 public interface PurchaseQueryRepository {
     List<Purchase> findByMember(Member byMember, @Nullable Boolean isComplete);
+
+    List<Purchase> findByItemSizes(ItemSizes itemSizes, @Nullable Boolean isComplete);
 }
