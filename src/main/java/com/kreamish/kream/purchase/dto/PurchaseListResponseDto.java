46a744acc1553a310e67399bd835ced4c1ed8a2c
kangyunsik <re12io@ajou.ac.kr>
2023-12-02 16:21:19 +0900

    KS-30 fix: 리플렉션을 위한 private noArgs 생성자 생성

--- src/main/java/com/kreamish/kream/purchase/dto/PurchaseListResponseDto.java
+++ src/main/java/com/kreamish/kream/purchase/dto/PurchaseListResponseDto.java
@@ -1,11 +1,14 @@
 package com.kreamish.kream.purchase.dto;
 
 import java.util.List;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PurchaseListResponseDto {
     List<PurchaseDetailResponseDto> purchases;
 }
