d6010a0730d3a3e888d8e8bf48db844edea830ba
kangyunsik <re12io@ajou.ac.kr>
2023-12-02 13:35:23 +0900

    KS-30 refact: Setter 대신, 생성자로 생성하도록 변경

--- src/main/java/com/kreamish/kream/purchase/dto/PurchaseListResponseDto.java
+++ src/main/java/com/kreamish/kream/purchase/dto/PurchaseListResponseDto.java
@@ -1,9 +1,11 @@
 package com.kreamish.kream.purchase.dto;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class PurchaseListResponseDto {
     List<PurchaseDetailResponseDto> purchases;
 }
